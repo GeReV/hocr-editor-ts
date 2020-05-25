a0d7b88895b1b0f224395e9d8632323715d633a7
Amir Grozki <amirgrozki@gmail.com>
2020-05-26 01:12:26 +0300

    Added the Header component.

---
+++ src/components/Header/index.tsx
@@ -0,0 +1,18 @@
+﻿import React, { PropsWithChildren } from "react";
+import cx from 'classnames';
+
+import './index.scss';
+
+interface Props {
+  className?: string;
+}
+
+function Header({ children, className }: PropsWithChildren<Props>) {
+  return (
+    <header className={cx('Header', className)}>
+      {children}
+    </header>
+  );
+}
+
+export default React.memo(Header);
