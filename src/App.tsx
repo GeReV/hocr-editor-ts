f67fe02ec03b684fe8465e2bcf8f5fee3afe8cd3
Amir Grozki <amirgrozki@gmail.com>
2020-06-08 15:17:31 +0300

    Removed unused import.

--- src/App.tsx
+++ src/App.tsx
@@ -8,7 +8,6 @@ import {
 import { useKey } from "react-use";
 import { library } from '@fortawesome/fontawesome-svg-core';
 import { fas } from '@fortawesome/free-solid-svg-icons';
-import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
 
 import { OcrDocument } from "./reducer/types";
 import { useAppReducer } from "./reducerContext";
