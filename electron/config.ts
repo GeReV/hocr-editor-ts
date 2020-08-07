9953cf1513cff9734091d19194b1b69fb31f4a0d
Amir Grozki <amirgrozki@gmail.com>
2020-08-07 22:43:47 +0300

    Initial work on app config for Electron build.

---
+++ electron/config.ts
@@ -0,0 +1,16 @@
+﻿import fs from 'fs';
+import path from 'path';
+
+interface Config {
+  tesseractPath: string;
+}
+
+export function getConfig(): Config {
+  const json = fs.readFileSync(path.resolve(__dirname, '../config.json'), { encoding: 'utf-8' });
+
+  return JSON.parse(json);
+}
+
+export default {
+  get: getConfig,
+}
